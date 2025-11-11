13384691b38f7b959cfe8ba8138471a225a2c6e9
Sakiih <rasyaakml136@gmail.com>
2025-11-11 12:54:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("helloBtn").addEventListener("click", function() {
+    const msg = document.getElementById("msg");
+    msg.innerText = "Yay! Kamu udah bilang hai 🐰✨";
+    msg.style.color = "#ff6fae";
+});
